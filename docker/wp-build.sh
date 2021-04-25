1c00dc647acdc8b98221f442a932844cf668acd5
h455257166 <455257166@qq.com>
2021-04-25 18:53:32 +0800

    Update wp-build.sh

--- docker/wp-build.sh
+++ docker/wp-build.sh
@@ -11,9 +11,12 @@ function download_yml() {
 echo -e "\n========================1. 创建目录========================\n"
 #判断liunx系统下/opt/wordpress/zjzx是否存在，如果不存在，则创建该目录层级
 [ ! -d /opt/wordpress/zjzx ] && mkdir -p /opt/wordpress/zjzx
+echo -e "\n创建完成\n"
 
 echo -e "\n========================2. 下载文件========================\n"
 download_yml
+echo -e "\n创建完成\n"
 
 echo -e "\n========================3. 创建镜像========================\n"
-wp-compose up -d
+#不使用-f参数指定文件，docker-compose会默认寻找名为docker-compose.yml 、docker-compose.yaml的配置文件
+docker-compose -f wp-compose.yml up -d
